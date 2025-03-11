a7591df33ef5822f6dfdc4bd0a6fc7c6f203e402
doingb <hjyeapkfvb76@hotmail.com>
2025-03-11 15:16:19 +0800

    init react+ts project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MockTwitter</h1>
+        <p>A React + TypeScript Twitter clone</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
